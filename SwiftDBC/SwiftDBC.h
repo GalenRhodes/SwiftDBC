469b9537892e5a2a90c1d17c6f7c36b124dce82d
Galen Rhodes <galenrhodes@me.com>
2020-08-25 07:31:50 -0400

    Initial Commit

---
+++ SwiftDBC/SwiftDBC.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDBC.h
+//  SwiftDBC
+//
+//  Created by Galen Rhodes on 8/25/20.
+//  Copyright © 2020 Project Galen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftDBC.
+FOUNDATION_EXPORT double SwiftDBCVersionNumber;
+
+//! Project version string for SwiftDBC.
+FOUNDATION_EXPORT const unsigned char SwiftDBCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDBC/PublicHeader.h>
+
+
